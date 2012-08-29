e85a29a81a4d93bd9e5d6e5058dd7176855ef703
Alex Weber <alexweber15@gmail.com>
2012-08-29 00:20:08 -0300

    refactored api and ditched class approach

--- meteologic.api.php
+++ meteologic.api.php
@@ -10,13 +10,21 @@
 function hook_meteologic_provider_info() {
   return array(
     'foo' => array(
+      // Name.
       'name' => t('Foo Provider'),
-      'class' => 'MeteologicFoo',
+      // Settings form callback.
       'settings' => 'foo_settings_form',
+      // Feeds implemented.
       'feeds' => array(
+        // Feed slug.
         'current' => array(
+          // Feed path.
           'path' => 'http://example.com/foo',
         ),
+      ),
+      // Stations callback.
+      'stations' => array(
+
       ),
     ),
   );
