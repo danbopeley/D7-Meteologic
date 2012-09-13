bf8b38ae4756f00e84915270b9e597da90d8ff16
Alex Weber <alexweber15@gmail.com>
2012-09-12 22:13:15 -0300

    huge improvement to api docs

--- meteologic.api.php
+++ meteologic.api.php
@@ -10,29 +10,54 @@
 function hook_meteologic_provider_info() {
   return array(
     'foo' => array(
-      // Name.
+      // Provider name.
       'name' => t('Foo Provider'),
-      // Settings form callback.
+      // Settings form callback, this is optional but can be useful to allow
+      // users to customize placeholder tokens (see below).
       'settings' => 'foo_settings_form',
-      // Feeds implemented.
+      // An array of feeds implemented, where the key is the feed's slug and
+      // the array contains an array with information.
       'feeds' => array(
         // Feed slug.
         'current' => array(
           // Feed path.
+          // The path is always available as a variable with the name:
+          // "meteologic_provider_path", where "provider" is the slug of
+          // the provider.
+          //
+          // For example: "meteologic_foo_path".
           'path' => 'http://example.com/foo',
+
+          // Feed tokens. These are values that will get replaced by
+          // customizable variables in the provider's settings. The
+          // corresponding variable name MUST be "meteologic_provider_token",
+          // where "provider" is the slug of the provider and "token is the
+          // name of the token minus the @.
+          //
+          // For example, for @station, the variable name will be:
+          // "meteologic_foo_station".
+          'tokens' => array('@station'),
+
           // Theme functions.
-          // Specify a callback function for both themes and blocks. If there's
-          // no change just specify the same one twice.
-          // 
+          // Specify either a single callback function or one for each of pages
+          // and blocks.
+          //
           // NOTE: You do not have to implement hook_theme() yourself
           // but you DO have to place these callbacks in a file called:
           // "MODULENAME.theme.inc", located in the module's root directory.
-          'theme' => array(
-            'block' => 'foo_current_block',
-            'page' => 'foo_current_page',
-          ),
+          // Note that the callback must be prefixe with "theme_".
+          //
+          // For example: "theme_foo_current".
+          'theme' => 'foo_current',
+          // Alternatively, you can specify different theme callbacks for pages
+          // and blocks:
+//          'theme' => array(
+//            'block' => 'foo_current_block',
+//            'page' => 'foo_current_page',
+//          ),
         ),
       ),
+
       // Stations callback.
       // You most likely just need to implement the "weather" callback.
       // However, some providers have different stations for radar feeds.
